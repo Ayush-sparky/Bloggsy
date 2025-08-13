9802ad8a78e3855ca53767504515500745c1b0ee
Ayush-sparky <ayushdk5758967@gmail.com>
2025-08-13 16:15:35 +0545

    feat: Added a secure login feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,41 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-const loginUser = async (req, res, next) => {};
+const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    const error = new Error("Missing email or password");
+    error.statusCode = 400;
+    return next(err);
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      const error = new Error("Invalid email or password");
+      error.statusCode = 400;
+      return next(err);
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      const error = new Error("Invalid email or password");
+      error.statusCode = 400;
+      return next(err);
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({
+      message: "Logged in successfully",
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   registerUser,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {registerUser, loginUser} = require('../controllers/userController')
 
+// Register new Users 
 router.post('/register', registerUser)
+
+// Login in users
+router.post('/login', loginUser)
 
 module.exports = router
