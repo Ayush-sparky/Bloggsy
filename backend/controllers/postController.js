169bf32d0af6fdb642c11c87faab7ce6bc43985f
Ayush-sparky <ayushdk5758967@gmail.com>
2025-08-13 18:06:52 +0545

    feat: implement createPost controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,21 +1,35 @@
-const postModel = require('../models/postModel')
+const postModel = require("../models/postModel");
 
 const createPost = async (req, res, next) => {
-    const {title, content} = req.body
-}
+  const { title, content } = req.body;
 
-const getAllPosts = async (req, res, next) => {
-    
-}
-const updatePost = async (req, res, next) => {
-    
-}
-const deletePost = async (req, res, next) => {
-    
-}
+  if (!title || !content) {
+    const error = new Error("All fields are required");
+    error.statusCode = 400;
+    return next(err);
+  }
 
+  try {
+    const newPost = await postModel.create({
+      title,
+      content,
+      author: req.user.id,
+      coverImage: req.file ? `/uploads/${req.file.filename}` : null,
+    });
 
+    res.status(201).json({
+      message: "New Post created successfully",
+      newPost,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getAllPosts = async (req, res, next) => {};
+const updatePost = async (req, res, next) => {};
+const deletePost = async (req, res, next) => {};
 
 module.exports = {
-    createPost
-}
+  createPost,
+};
