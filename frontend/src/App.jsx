9c3076e8e671f0dfc40fe28aab7473fcf4937034
Ayush-sparky <ayushdk5758967@gmail.com>
2025-08-15 23:45:27 +0545

    test: Testing some backend API's

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <h1>Hello This is a fullstack blog App </h1>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(
+          "http://localhost:3030/api/posts?page=1&limit=5"
+        );
+        const data = await res.json();
+        setPosts(data.allPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <h1>Hello This is a fullstack blog App </h1>
+      <ul>
+        {posts.map((post) => {
+          return <li>{post.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
